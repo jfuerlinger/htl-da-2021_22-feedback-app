eeb05f51ce15679bd4535185e4142425c8010eec
Mirzet Sakonjic <55495064+Mirzet24@users.noreply.github.com>
2021-11-27 16:42:28 +0100

    Upgrade

---
+++ RealAPiTuts64/RealAPiTuts64/RealAPI/Model/Login/LoginApiRequestModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RealAPI.Model.Login
+{
+    public class LoginApiRequestModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
---
+++ RealAPiTuts64/RealAPiTuts64/RealAPI/Model/Login/LoginApiResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace RealAPI.Model.Login
+{
+   
+    public class User
+    {
+        public string userId { get; set; }
+    }
+
+    public class LoginApiResponseModel
+    {
+        public string authenticationToken { get; set; }
+        public User user { get; set; }
+    }
+}
