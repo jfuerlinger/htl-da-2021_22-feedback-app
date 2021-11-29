b723a5315dac7a4982d94c5ec2313b84987e9b21
Mirzet Sakonjic <55495064+Mirzet24@users.noreply.github.com>
2021-11-29 14:42:14 +0100

    username UserName

--- FeedbackApp_WebApi/Authentication/LoginModel.cs
+++ FeedbackApp_WebApi/Authentication/LoginModel.cs
@@ -9,7 +9,7 @@ namespace FeedbackApp_WebApi.Authentication
     public class LoginModel
     {
         [Required(ErrorMessage = "Username ist erforderlich")]
-        public string UserName { get; set; }
+        public string Username { get; set; }
 
         [Required(ErrorMessage = "Passwort ist erforderlich")]
         public string Password { get; set; }
