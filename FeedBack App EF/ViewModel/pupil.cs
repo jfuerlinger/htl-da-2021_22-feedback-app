585f820b4c4b62c7a3c4f7aad7d4c6a5f729fb20
Stefano Pyringer <py.stefano@gmail.com>
2021-12-15 17:33:45 +0100

    add EF Project

---
+++ FeedBack App EF/ViewModel/pupil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.ViewModel
+{
+    public class pupil
+    {
+        public int ID{ get; set; }
+        public string firstname{ get; set; }
+        public string lastname{ get; set; }
+        public string username{ get; set; }
+        public string emailaddress{ get; set; }
+        public string school{ get; set; }
+    }
+}
