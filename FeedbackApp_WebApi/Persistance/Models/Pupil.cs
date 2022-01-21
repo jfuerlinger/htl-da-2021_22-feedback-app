b564af2ee41641ffad2b55abe35e0676008ed67e
Stefano Pyringer <py.stefano@gmail.com>
2022-01-21 21:35:12 +0100

    add pupil and teacher model

---
+++ FeedbackApp_WebApi/Persistance/Models/Pupil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackApp_WebApi.Persistance.Models
+{
+    public class Pupil
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string School { get; set; }
+    }
+}
---
+++ FeedbackApp_WebApi/Persistance/Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackApp_WebApi.Persistance.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string School { get; set; }
+    }
+}
