3f02ad8893fc85a92e584f5e151c9e282031611e
Stefano Pyringer <py.stefano@gmail.com>
2022-03-01 11:36:36 +0100

    add model classes

---
+++ FeedbackApp.Core/Model/Feedback.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackApp.Core.Model
+{
+    public class Feedback
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string SubscriptionKey { get; set; }
+
+    }
+}
---
+++ FeedbackApp.Core/Model/Review.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackApp.Core.Model
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public int Stars { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
